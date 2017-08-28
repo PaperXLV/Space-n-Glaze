c103022d3c314018a8bb1344f666d697e168d8a6
Sam Williams <sam@ukethat.com>
2017-08-28 00:34:51 +0000

    added cart view and controller

---
+++ app/controllers/carts_controller.rb
@@ -0,0 +1,8 @@
+class CartsController < ApplicationController 
+   def new
+   end
+   
+   def index
+   end
+    
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about', to: 'pages#about'
   resources :contacts, only: :create
+  resources :carts
   get 'contact-us', to: 'contacts#new', as: 'new_contact'
   get 'shop', to: 'pages#shop'
 end
