ecbc5c5cd623605e5ea8b99b0f81e14bddcdfc54
Sam Williams <sam@ukethat.com>
2017-09-05 23:18:11 +0000

    changes to ordering

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -2,12 +2,7 @@ class OrderItemsController < ApplicationController
   def create
     @order = current_order
     @order_item = @order.order_items.new(order_item_params)
-    existing_order = @order.order_items.where(product_id: params[:orderitem][:product_id])
-    if existing_order.count >= 1
-      existing_order.last.update_column(:quantity, existing_order.last.quantity + params[:order_item][:quantity].toi)
-    else
-      @order.save
-    end
+    @order.save
     session[:order_id] = @order.id
   end
 
